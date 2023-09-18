dada3ed96e5481dcec5e986ecc589a7eb57dc4a5
AlexGuoussatchok <115575196+AlexGuoussatchok@users.noreply.github.com>
2023-09-18 18:20:19 +0300

    + cameras catalogue brands + models implemented

--- lib/screens/cameras_catalogue_screen.dart
+++ lib/screens/cameras_catalogue_screen.dart
@@ -13,6 +13,8 @@ class CamerasCatalogueScreen extends StatefulWidget {
 
 class _CamerasCatalogueScreenState extends State<CamerasCatalogueScreen> {
   List<String> cameraBrands = []; // Store unique camera brands here
+  Map<String, List<String>> brandModels = {}; // Store models for each brand
+  Map<String, bool> isExpanded = {}; // Store expansion state for each brand
 
   @override
   void initState() {
@@ -51,9 +53,29 @@ class _CamerasCatalogueScreenState extends State<CamerasCatalogueScreen> {
     final brands = result.map((row) => row['camera_brand'] as String).toList();
     setState(() {
       cameraBrands = brands;
+      for (final brand in brands) {
+        isExpanded[brand] = false; // Initialize all brands as collapsed
+      }
     });
 
     // Close the database
+    await database.close();
+  }
+
+  Future<void> fetchCameraModels(String brand) async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'catalogue.db');
+    final database = await openDatabase(path, readOnly: true);
+
+    final tableName = '${brand.toLowerCase()}_cameras_catalogue';
+    final result = await database.rawQuery('''
+      SELECT camera_name
+      FROM $tableName
+    ''');
+
+    final models = result.map((row) => row['camera_name'] as String).toList();
+    brandModels[brand] = models;
+
     await database.close();
   }
 
@@ -69,43 +91,80 @@ class _CamerasCatalogueScreenState extends State<CamerasCatalogueScreen> {
           itemCount: cameraBrands.length,
           itemBuilder: (context, index) {
             final brand = cameraBrands[index];
-            return Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.black, // Border color
-                    width: 1.0, // Border width
-                  ),
-                ),
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Handle selection change if needed
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black, backgroundColor: Colors.grey, // Text color
-                  padding: const EdgeInsets.all(16.0), // Adjust padding
-                  alignment: Alignment.centerLeft, // Align text to the left
-                ),
-                child: DropdownButton<String>(
-                  value: brand, // Set the selected value
-                  onChanged: (value) {
-                    // Handle selection change if needed
-                  },
-                  items: [
-                    DropdownMenuItem(
-                      value: brand,
-                      child: Text(
-                        brand,
-                        style: const TextStyle(
-                          color: Colors.black, // Text color
-                          fontSize: 24, // Adjust font size
-                        ),
+            return Column(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Colors.black, // Border color
+                        width: 1.0, // Border width
                       ),
                     ),
-                  ],
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (!isExpanded[brand]!) {
+                        await fetchCameraModels(brand);
+                      }
+                      setState(() {
+                        isExpanded[brand] = !isExpanded[brand]!;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      onPrimary: Colors.black,
+                      padding: const EdgeInsets.all(16.0),
+                      alignment: Alignment.centerLeft,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start, // Align text to the left
+                      children: [
+                        Text(
+                          brand,
+                          style: const TextStyle(
+                            fontSize: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+                if (isExpanded[brand]!)
+                  Column(
+                    children: brandModels[brand]!.map((model) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.black, // Border color
+                              width: 1.0, // Border width
+                            ),
+                          ),
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Handle selection change if needed
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.grey,
+                            onPrimary: Colors.black,
+                            padding: const EdgeInsets.all(16.0),
+                            alignment: Alignment.centerLeft,
+                          ),
+                          child: Center( // Center-align the model name
+                            child: Text(
+                              model,
+                              style: const TextStyle(
+                                fontSize: 32,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+              ],
             );
           },
         ),
