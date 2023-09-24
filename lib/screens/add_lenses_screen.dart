b9a07240b5ca69c9e78f9efe8a9cef1909e72ac1
AlexGuoussatchok <115575196+AlexGuoussatchok@users.noreply.github.com>
2023-09-24 10:10:30 +0300

    + add lenses screen - models names fetched & list of models sorted alphabetically in the drop down

--- lib/screens/add_lenses_screen.dart
+++ lib/screens/add_lenses_screen.dart
@@ -25,8 +25,10 @@ class _AddLensesScreenState extends State<AddLensesScreen> {
   final TextEditingController commentsController = TextEditingController();
 
   List<String> lensBrands = [];
+  List<String> lensModels = [];
 
   String selectedBrand = '';
+  String selectedModel = '';
 
   Future<void> fetchLensBrands() async {
     final databasesPath = await getDatabasesPath();
@@ -42,7 +44,29 @@ class _AddLensesScreenState extends State<AddLensesScreen> {
     }
 
     lensBrands = uniqueBrands.toList();
-    lensBrands.sort(); // Sort the lensBrands alphabetically
+    lensBrands.sort();
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<void> fetchLensModels(String brand) async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'catalogue.db');
+    final db = await openDatabase(path);
+
+    final tableName = '${brand.toLowerCase()}_lenses_catalogue'; // Generate the table name
+    final models = await db.query(tableName, columns: ['model']);
+    final uniqueModels = <String>{};
+
+    for (final modelMap in models) {
+      final model = modelMap['model'].toString();
+      uniqueModels.add(model);
+    }
+
+    lensModels = uniqueModels.toList();
+    lensModels.sort();
 
     if (mounted) {
       setState(() {});
@@ -79,13 +103,26 @@ class _AddLensesScreenState extends State<AddLensesScreen> {
               onChanged: (String? value) {
                 setState(() {
                   selectedBrand = value!;
+                  lensModels = []; // Clear the previous models when the brand changes
                 });
+                fetchLensModels(selectedBrand); // Fetch lens models based on the selected brand
               },
               decoration: const InputDecoration(labelText: 'Brand'),
             ),
 
-            TextFormField(
-              controller: modelController,
+            DropdownButtonFormField<String>(
+              value: selectedModel.isNotEmpty && lensModels.contains(selectedModel) ? selectedModel : null,
+              items: lensModels.map((String model) {
+                return DropdownMenuItem<String>(
+                  value: model,
+                  child: Text(model),
+                );
+              }).toList(),
+              onChanged: (String? value) {
+                setState(() {
+                  selectedModel = value ?? '';
+                });
+              },
               decoration: const InputDecoration(labelText: 'Model'),
             ),
             TextFormField(
